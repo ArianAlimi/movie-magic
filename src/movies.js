00c2bfa6ae4bacb936cee3b38fa693862914da20
ArianAlimi <sparklingpinky123@gmail.com>
2025-02-01 00:13:38 +0200

    Add dynamic movie rendering on home page

---
+++ src/movies.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        title: 'Jungle Croise',
+        genre: 'Adventure',
+        description: 'Description: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
+        imageUrl: './static/img/jungle-cruise.jpeg'
+    },
+
+    {
+        title: 'Rick and Morty',
+        genre: 'Sci-Fi',
+        description: "The fractured domestic lives of a nihilistic mad scientist and his anxious grandson are further complicated by their inter-dimensional misadventures.",
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BZGQyZjk2MzMtMTcyNC00NGU3LTlmNjItNDExMWM4ZDFhYmQ2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
+    }
+]
