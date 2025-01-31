7e8c863649b600cfd15d60d93788773b05d0ab15
ArianAlimi <sparklingpinky123@gmail.com>
2025-01-31 23:41:27 +0200

    Add routes

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
 
-const router = Router();
+const routes = Router();
 
-router.get('/', (req, res) => {
+routes.get('/', (req, res) => {
     res.render('home');
 });
 
-router.get('/about', (req, res) => {
+routes.get('/about', (req, res) => {
     res.render('about');
 });
 
-export default router;
+routes.get('/create', (req, res) => {
+    res.render('create');
+});
+
+routes.get('/search', (req, res) => {
+    res.render('search');
+});
+
+
+export default routes;
