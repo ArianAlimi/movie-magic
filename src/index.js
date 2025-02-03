99c1f92aa526f6a17d158bf1552b49a6f4d85f5d
ArianAlimi <sparklingpinky123@gmail.com>
2025-02-03 20:59:03 +0200

    Parse form data on server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import routes from './routes.js'
+import routes from './routes.js';
 
 const app = express();
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use('/static', express.static('./src/public'))
+app.use('/static', express.static('./src/public'));
+app.use(express.urlencoded({extended: false}));
 
 app.use(routes);
 
-app.listen(5000, () => console.log('http://localhost:5000...'))
+app.listen(5000, () => console.log('http://localhost:5000...'));
--- src/movies.js
+++ src/movies.js
@@ -4,7 +4,12 @@ export default [
         title: 'Jungle Croise',
         genre: 'Adventure',
         description: 'Description: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
-        imageUrl: './static/img/jungle-cruise.jpeg'
+        imageUrl: '/static/img/jungle-cruise.jpeg',  
+        director: 'Joro',
+        year: '2020',
+        rating: '5',
+        category: 'Movie',
+      
     },
 
     {
@@ -12,6 +17,10 @@ export default [
         title: 'Rick and Morty',
         genre: 'Sci-Fi',
         description: "The fractured domestic lives of a nihilistic mad scientist and his anxious grandson are further complicated by their inter-dimensional misadventures.",
-        imageUrl: 'https://m.media-amazon.com/images/M/MV5BZGQyZjk2MzMtMTcyNC00NGU3LTlmNjItNDExMWM4ZDFhYmQ2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BZGQyZjk2MzMtMTcyNC00NGU3LTlmNjItNDExMWM4ZDFhYmQ2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+        director: 'Justin Roiland',
+        year: '2010',
+        rating: '5',
+        category: 'Serie',
     }
 ]
